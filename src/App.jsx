1469cae49555c3826acc3ba604e0d5d060560126
deveshanim3 <deveshsingh6986@gmail.com>
2026-02-13 00:14:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Grid from './assets/components/Grid'
+import './App.css'
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Grid/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Grid.jsx
@@ -0,0 +1,48 @@
+import React, { use, useState } from 'react'
+import Box from './Box'
+
+const Grid = () => {
+    const [grid,setGrid]=useState(Array(9).fill(0))
+    const handleClick=(i)=>{
+        setGrid(prev=>{
+            if(prev[i]>=15)return prev;
+
+            const next=[...prev];
+            next[i]+=1
+            const val=next[i];
+            const row=Math.floor(i/3)
+            const col=i%3
+
+            if(val%3==0){
+                if(col<2){
+                    const rightIdx=i+1
+                    if(prev[rightIdx]<15){
+                        next[rightIdx]+=1
+                    }
+                }
+            }
+            if(val%5==0){
+                if(row<2){
+                    const downIdx=i+3
+                    if(prev[downIdx]<15){
+                        next[downIdx]+=2
+                    }
+                }
+            }
+            return next
+        })
+    }
+    return (
+        <div className='grid grid-cols-3 grid-rows-3 gap-2'>
+            {
+                grid.map((value,idx)=>{
+                    return(
+                        <Box key={idx} value={value} onClick={()=>handleClick(idx)} />
+                            )
+                        })
+            }
+        </div>
+    )
+}
+
+export default Grid
