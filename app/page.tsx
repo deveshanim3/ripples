6bda0a5b0b9575e9bd1ddb794c7efecb4b870980
deveshanim3 <deveshsingh6986@gmail.com>
2026-02-13 23:39:45 +0530

    changed title

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Grid from "../components/Grid";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-600">
-      
+    <div className="flex justify-center items-center h-screen bg-slate-600">     
       <Grid/>
     </div>
   );
